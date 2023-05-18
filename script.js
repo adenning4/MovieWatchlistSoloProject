3fc03ccc998bc6732291072510e5cafc962d6f8d
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-05-18 12:27:18 -0600

    initial search functionality
    - search function works and generates a list of movie posters

--- script.js
+++ script.js
@@ -3,17 +3,56 @@ const apiKey = "f5d1bb34";
 
 const searchInputEl = document.getElementById("search-input-el");
 const searchButtonEl = document.getElementById("search-button-el");
+const searchResultsEl = document.getElementById("search-results");
 
-searchButtonEl.addEventListener("click", performSearch);
+searchButtonEl.addEventListener("click", searchTitles);
 
-function performSearch() {
+async function searchTitles() {
   if (searchInputEl.value) {
-    const searchWord = searchInputEl.value.toLowerCase();
+    const searchTerm = searchInputEl.value.toLowerCase();
 
-    const url = `${baseUrl}?apikey=${apiKey}&s=${searchWord}`;
+    const titleSearchUrl = `${baseUrl}?apikey=${apiKey}&s=${searchTerm}`;
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+    const response = await fetch(titleSearchUrl);
+    const data = await response.json();
+
+    handleDataResponse(data);
   }
+}
+
+async function handleDataResponse(data) {
+  if (data.Response === "True") {
+    const imdbIDs = [];
+    data.Search.forEach((film) => {
+      imdbIDs.push(film.imdbID);
+    });
+    const filmResultsHtml = await getFilmResultsHtml(imdbIDs);
+    searchResultsEl.innerHTML = filmResultsHtml;
+  } else {
+    searchResultsEl.innerHTML = `
+          <div class="empty-search-results">
+              Unable to find what you're looking for. Please try another search.
+          </div>
+      `;
+  }
+}
+
+async function getFilmResultsHtml(ids) {
+  let html = "";
+
+  for (let id of ids) {
+    const imdbIdSearchUrl = `${baseUrl}?apikey=${apiKey}&i=${id}`;
+
+    const response = await fetch(imdbIdSearchUrl);
+    const data = await response.json();
+    html += `
+          <section class="film-summary">
+              <div class="film-poster">
+                  <img src="${data["Poster"]}" />
+              </div>
+          </section>
+          `;
+  }
+
+  return html;
 }
