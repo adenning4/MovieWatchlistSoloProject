97358e08ece002b744bf8d45d151cb2d5311bd02
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-05-18 19:38:52 -0600

    Local storage functions
    Upon clicking watchlist button...
    -Adds an id to local storage if not present
    -Initializes local storage if it doesn't exist
    -Removes an id from local storage if already present

--- script.js
+++ script.js
@@ -5,7 +5,50 @@ const searchInputEl = document.getElementById("search-input-el");
 const searchButtonEl = document.getElementById("search-button-el");
 const searchResultsEl = document.getElementById("search-results");
 
-searchButtonEl.addEventListener("click", searchTitles);
+document.addEventListener("click", (e) => {
+  if (e.target.id === "search-button-el") {
+    searchTitles();
+  } else if (e.target.className === "watchlist-button") {
+    watchlistUpdate(e);
+  }
+});
+
+function watchlistUpdate(e) {
+  // localStorage.setItem("imdbIds", e.target.dataset.imdbId);
+  updateLocalStorage(e.target.dataset.imdbId);
+}
+
+function updateLocalStorage(id) {
+  // if id in local storage, remove it!
+  if (isIdInLocalStorage(id)) {
+    const localIds = JSON.parse(localStorage.getItem("localIds"));
+    const idIndexInLocalIds = localIds.indexOf(id);
+    localIds.splice(idIndexInLocalIds, 1);
+    localStorage.setItem("localIds", JSON.stringify(localIds));
+  }
+  // if id not in local storage, add it!
+  else {
+    if (localStorageExists()) {
+      const localIds = JSON.parse(localStorage.getItem("localIds"));
+      localIds.push(id);
+      localStorage.setItem("localIds", JSON.stringify(localIds));
+      // if local storage doesn't exist, initialize it!
+    } else {
+      const initLocalStorage = JSON.stringify([id]);
+      localStorage.setItem("localIds", initLocalStorage);
+    }
+  }
+}
+
+function isIdInLocalStorage(id) {
+  const currentLocalStorage = JSON.parse(localStorage.getItem("localIds"));
+  return localStorageExists() && currentLocalStorage.includes(id);
+}
+
+//short function used for sake of readability
+function localStorageExists() {
+  return !!localStorage.getItem("localIds");
+}
 
 async function searchTitles() {
   if (searchInputEl.value) {
@@ -59,7 +102,11 @@ async function getFilmResultsHtml(ids) {
                 <div class="middle">
                   <p class="film-details">${data["Runtime"]}</p>
                   <p class="film-details">${data["Genre"]}</p>
-                  <button class="watchlist-button">Watchlist</button>
+                  <button 
+                    data-imdb-id="${id}" 
+                    class="watchlist-button">
+                    Watchlist
+                  </button>
                 </div>
                 <div class="bottom">
                   <p class="film-plot">${data["Plot"]}</p>
